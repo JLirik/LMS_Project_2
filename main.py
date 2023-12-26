d9fb282ff2aae670a4286565175ff519dec62cd9
JLirik <dimdimych181007@yandex.ru>
2023-12-27 00:26:03 +0300

    Uno Map Commit

--- main.py
+++ main.py
@@ -77,3 +77,91 @@ def start_screen():  # Создание экрана
 
 
 start_screen()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+    print(max_width)
+
+    # дополняем каждую строку пустыми клетками ('=') + убираем ненужные клетки
+    lvl_1 = list(map(lambda x: x.ljust(max_width, '='), level_map))
+
+    return lvl_1
+
+
+level = load_level('lvl1.txt')
+# print(level)
+
+
+tile_images = {
+    # 'wall': load_image('textura_stena.jpg'),
+    # 'empty': load_image('textura_pol.jpg'),
+    # 'inwall': load_image('textura_V_stene_1.jpg')
+    'wall': load_image('textura_1_stena.jpg'),
+    'empty': load_image('textura_1_pol.jpg'),
+    'inwall': load_image('textura_1_V_stene.jpg')
+}
+# player_image = load_image('mar.png')
+
+tile_width = tile_height = 15
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+def generate_level(level):
+    # new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '=':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '~':
+                Tile('inwall', x, y)
+            # elif level[y][x] == '@':
+            #     Tile('empty', x, y)
+            #     new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    # return new_player, x, y
+    return x, y
+
+
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+print(0)
+level_x, level_y = generate_level(load_level('lvl1.txt'))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT and level[player.rect.y // 50][player.rect.x // 50 - 1] != '#':
+        #         player.rect.x -= 50
+        #     elif event.key == pygame.K_RIGHT and level[player.rect.y // 50][player.rect.x // 50 + 1] != '#':
+        #         player.rect.x += 50
+        #     elif event.key == pygame.K_UP and level[player.rect.y // 50 - 1][player.rect.x // 50] != '#':
+        #         player.rect.y -= 50
+        #     elif event.key == pygame.K_DOWN and level[player.rect.y // 50 + 1][player.rect.x // 50] != '#':
+        #         player.rect.y += 50
+
+    screen.fill(pygame.Color("white"))
+    all_sprites.draw(screen)
+    tiles_group.draw(screen)
+    # player_group.draw(screen)
+    pygame.display.flip()
+    clock.tick(10)
+
+pygame.quit()
--- photo_map.py
+++ photo_map.py
@@ -1,3 +1,62 @@
+# from PIL import Image, ImageDraw
+#
+# karta = []
+# f = open('data\lvl1.txt', 'w')
+#
+#
+# # Free cells - rgb(40, 150, 40) - '='
+# # Walls ---- rgb(200, 200, 16) - '#'
+# # Another walls - rgb(255, 255, 0)
+# # Wall cells - rgb(147, 112, 219) - '~'
+# # Beween cells - rgb(255, 0, 127) - '|'
+#
+#
+# def board():
+#     global karta
+#     im = Image.open("data\level1_1.png")
+#     pixels = im.load()  # список с пикселями
+#     x, y = im.size  # ширина (x) и высота (y) изображения
+#     symb = ''
+#     b = 0
+#     c = 0
+#
+#     for j in range(0, y, 1):
+#         karta.append([])
+#         h = 0
+#         for i in range(0, x, 1):
+#             r, g, b = pixels[i, j][:-1]
+#             if r == 40 and g == 150 and b == 40:
+#                 symb = '='
+#             elif r == 255 and g == 255 and b == 0:
+#                 symb = '#'
+#             elif r == 255 and g == 0 and b ==127:
+#                 symb = '|'
+#             elif r == 147 and g == 112 and b == 219:
+#                 symb = '~'
+#             else:
+#                 symb = '~'
+#             # karta[c].append(symb)
+#             if h != 0:
+#                 if karta[c][h - 1] != symb:
+#                     karta[c].append(symb)
+#                     h += 1
+#             else:
+#                 karta[c].append(symb)
+#                 h += 1
+#         c += 1
+#     return karta
+#
+#
+# a = board()
+# # a = a[92:2210]
+# for e in a:
+#     # e = e[105:-95]
+#     e = (''.join(e)).split('|')
+#     if len(e) > 0:
+#         f.write(''.join(e) + '\n')
+# print(len(a))
+# f.close()
+
 from PIL import Image, ImageDraw
 
 karta = []
@@ -16,9 +75,9 @@ def board():
     x, y = im.size  # ширина (x) и высота (y) изображения
     c = 0
 
-    for j in range(0, y, 1):
+    for j in range(0, y, 10):
         karta.append([])
-        for i in range(0, x, 1):
+        for i in range(0, x, 9):
             r, g, b = pixels[i, j][:-1]
             if r == 40 and g == 150 and b == 40:
                 karta[c].append('=')
@@ -33,8 +92,10 @@ def board():
 
 
 a = board()
+a = a[10:221]
 for e in a:
-    print(e)
+    e = e[12:-11]
     f.write(''.join(e) + '\n')
 print(len(a))
 f.close()
+
